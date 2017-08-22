7dd8ab85e40df32b6ba1190d6a4437defa608bad
jiaxiaoyan <jiaxiaoyan@syswin.com>
2017-08-22 10:00:00 +0800

    add restruction

--- XYCircleLoading/CircleLoadingView.swift
+++ XYCircleLoading/CircleLoadingView.swift
@@ -20,16 +20,19 @@ class CircleLoadingView: UIView,CAAnimationDelegate{
     }
 //两秒转三圈
     func normalLoading() {
+        //layer动画的运行的过程：开始执行动画时，隐藏UIView原有显示layer，创建一个presentLayer，由presentlayer去执行动画，动画结束移除presentlayer，显示原有layer
+        
         self.loadingImageView.isHidden = false
-        let animation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        animation.duration = 0.7
-        animation.repeatCount = 3
-        animation.autoreverses = false
-        animation.fromValue = 0
-        animation.toValue = Double.pi*2.0
-        animation.isRemovedOnCompletion = false
-        animation.fillMode = kCAMediaTimingFunctionLinear //匀速
-        animation.beginTime = CACurrentMediaTime()
+        let animation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")//实例化CABasicAnimation，用keyPath指定layer的属性
+        animation.duration = 0.7 //动画每执行一次所需时间
+        animation.repeatCount = 3 //重复次数,无限次循环是HUGE
+        animation.autoreverses = false//动画结束后是否执行逆动画
+        animation.fromValue = 0 //所改变属性的起始值
+        animation.toValue = Double.pi*2.0 //所改变属性的终止值
+        animation.isRemovedOnCompletion = false//动画结束是否移除动画效果
+        animation.fillMode = "forwards"//动画停止之后返回初始状态
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)//匀速，，，，，动画样式
+        animation.beginTime = CACurrentMediaTime() //如需延迟 CACurrentMediaTime() + 2
         animation.delegate = self;
         self.loadingImageView.layer.add(animation, forKey: "normalLoading")
     }
